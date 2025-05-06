94bba6ff33dca91cd307c2a479e5834f9b653754
GaoCXGithub <gaocx.work@gmail.com>
2025-05-05 18:30:44 -0700

    Update Project 7

--- 4 Unsupervised Machine Learning and TPOT/4-2 TPOT/tpot_census_pipeline.py
+++ 4 Unsupervised Machine Learning and TPOT/4-2 TPOT/tpot_census_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.8330876330876331
-exported_pipeline = XGBClassifier(learning_rate=0.01, max_depth=7, min_child_weight=11, n_estimators=100, n_jobs=1, subsample=0.1, verbosity=0)
+# Average CV score on the training set was: 0.7873054873054872
+exported_pipeline = SGDClassifier(alpha=0.01, eta0=1.0, fit_intercept=True, l1_ratio=1.0, learning_rate="constant", loss="modified_huber", penalty="elasticnet", power_t=50.0)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
